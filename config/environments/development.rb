46a83d9deaed5ea36925bef5cf3c8c9b208e8194
rasna shakya <rasnashakya@gmail.com>
2017-02-22 07:36:54 +0545

    added pic in devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,5 +55,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true 
+  # config.force_ssl = true
 
 end
