1acfaded6a51b07c860f3afef8792c1a09838603
rasna shakya <rasnashakya@gmail.com>
2017-02-22 11:49:07 +0545

    cleared cached from git, added .env

--- config/initializers/mailer_settings.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address                => 'smtp.gmail.com',
-  :domain                 => 'mail.google.com',
-  :port                   => 587,
-  :user_name              => ENV['GMAIL_USERNAME'],
-  :password               => ENV['GMAIL_PASSWORD'],
-  :authentication         => 'login',
-  :enable_starttls_auto   => true
-}
